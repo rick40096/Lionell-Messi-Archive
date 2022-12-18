ab5618344d532f8a3c59b918f65e7c398139af60
HashiraAssociation <118347352+HashiraAssociation@users.noreply.github.com>
2022-12-19 00:21:30 +0530

    Update blsticker_sql.py

--- Messi/modules/sql/blsticker_sql.py
+++ Messi/modules/sql/blsticker_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func, Integer
 
-from EmikoRobot.modules.sql import BASE, SESSION
+from Messi.modules.sql import BASE, SESSION
 
 
 class StickersFilters(BASE):
