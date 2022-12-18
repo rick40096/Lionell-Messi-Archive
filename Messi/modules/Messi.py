9ecfb0ba305b9a3bc8c06f30fd23aca77828f32e
HashiraAssociation <118347352+HashiraAssociation@users.noreply.github.com>
2022-12-18 20:21:25 +0530

    Create Messi.py

---
+++ Messi/modules/Messi.py
@@ -0,0 +1,21 @@
+import os
+import re
+import random
+
+from platform import python_version as kontol
+from telethon import events, Button
+from Messi.events import register
+from Messi import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/f8f96fc3fed17875a0673.jpg"
+
+@register(pattern=("awake"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Lionell Messi .** \n\n"
+  TEXT += f"❄Messi Is Alive 💘** \n\n"
+  TEXT += f"❄My Domain : [FIFA](https://t.me/Fifa_Federation)** \n\n"
+  TEXT += f"❄ **Powered By: [Messi Team](https://t.me/Messi_Probot_Team)** \n\n"
+   Thanks For Adding Me Here ❤️ re ❤️ **"
+  BUTTON = [[Button.url("Help", "https://t.me/messi_probot?start=help"), Button.url("My Home", "https://t.me/Messi_Probot_Support")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
