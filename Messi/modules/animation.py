64b506fc2d973442de49aea47ed03f04391e1881
HashiraAssociation <118347352+HashiraAssociation@users.noreply.github.com>
2022-12-22 19:29:28 +0530

    Update animation.py

--- Messi/modules/animation.py
+++ Messi/modules/animation.py
@@ -4,9 +4,9 @@ from typing import List
 from telegram import Update
 from telegram.ext import run_async, CallbackContext
 
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
-from EmikoRobot.modules.helper_funcs.chat_status import user_admin
+from Messi import dispatcher
+from Messi.modules.disable import DisableAbleCommandHandler
+from Messi.modules.helper_funcs.chat_status import user_admin
 
 # sleep how many times after each edit in 'love'
 EDIT_SLEEP = 1
