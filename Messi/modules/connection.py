529e64521a6846c262120c1b3303ac1ee07193a8
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-23 19:18:54 +0530

    Update connection.py

--- Messi/modules/connection.py
+++ Messi/modules/connection.py
@@ -5,10 +5,10 @@ from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton, Upda
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import CommandHandler, CallbackQueryHandler, run_async
 
-import TOGA.modules.sql.connection_sql as sql
-from TOGA import dispatcher, DRAGONS, DEV_USERS
-from TOGA.modules.helper_funcs import chat_status
-from TOGA.modules.helper_funcs.alternate import send_message, typing_action
+import Messi.modules.sql.connection_sql as sql
+from Messi import dispatcher, DRAGONS, DEV_USERS
+from Messi.modules.helper_funcs import chat_status
+from Messi.modules.helper_funcs.alternate import send_message, typing_action
 
 user_admin = chat_status.user_admin
 
