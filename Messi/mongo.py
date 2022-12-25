becb0289c7959434f919cc9d78894abe224427cd
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-25 18:00:37 +0530

    Update mongo.py

--- Messi/mongo.py
+++ Messi/mongo.py
@@ -16,9 +16,9 @@ MONGO_DB = "EmikoRobot"
 
 
 client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
-db = motor[MONGO_DB]
+client = MongoClient("mongodb+srv://archisman:Rick_2005@cluster0.ts8ihph.mongodb.net/?retryWrites=true&w=majority", 27017)[Messi]
+motor = motor_asyncio.AsyncIOMotorClient("mongodb+srv://archisman:Rick_2005@cluster0.ts8ihph.mongodb.net/?retryWrites=true&w=majority", 27017)
+db = motor[Messi]
 db = client["emiexrobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
