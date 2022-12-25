274f85d1d41f9b69bb68754ba56b057a5b5ec4f4
PrincesssGirlXD <120161303+PrincesssGirlXD@users.noreply.github.com>
2022-12-25 18:06:28 +0530

    Update mongo.py
    Omfoo 🥭🥭

--- Messi/mongo.py
+++ Messi/mongo.py
@@ -9,11 +9,12 @@ from pymongo.errors import ServerSelectionTimeoutError
 from Messi.config import *
 from Messi.confing import get_int_key, get_str_key
 
+MONGO_DB = "Messi"
 
 client = MongoClient()
-client = MongoClient("mongodb+srv://archisman:Rick_2005@cluster0.ts8ihph.mongodb.net/?retryWrites=true&w=majority", 27017)[Messi]
+client = MongoClient("mongodb+srv://archisman:Rick_2005@cluster0.ts8ihph.mongodb.net/?retryWrites=true&w=majority", 27017)[MONGO_DB]
 motor = motor_asyncio.AsyncIOMotorClient("mongodb+srv://archisman:Rick_2005@cluster0.ts8ihph.mongodb.net/?retryWrites=true&w=majority", 27017)
-db = motor[Messi]
+db = motor[MONGO_DB]
 db = client["emiexrobot"]
 try:
     asyncio.get_event_loop().run_until_complete(motor.server_info())
